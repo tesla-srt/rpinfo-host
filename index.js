22d36bda78898b2d5818a2d44573d10210af54c1
tesla-srt <71569052+tesla-srt@users.noreply.github.com>
2023-05-29 05:33:18 -0400

    NWJS --> NodeGui
    Moved from NWJS to NodeGui

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const { QSystemTrayIcon, QIcon } = require ('@nodegui/nodegui');
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const { fork, execSync } = require("child_process")
+const { fork, execSync } = require("child_process");
+const { resolve } = require("path");
 const io = new Server(server, {
   cors: {
     //origin: "chrome-extension://ophmdkgfcjapomjdpfobjfbihojchbko",
@@ -16,28 +18,20 @@ const cmd0 = 'taskkill /f /im eggplant.exe';
 
 let port = process.env.PORT || 3000;
 
-var gui = require('nw.gui');
-// Create a tray icon
-let tray = new gui.Tray({
-  title: 'HWMon',
-  tooltip: 'Nick\'s HW Monitoring App!',
-  icon: 'img/icon.png'
-});
-tray.on('click', function() {
-  //App.closeAllWindows();
-  //App.quit();
-  terminate();
-});
+const trayIcon = new QIcon("img/icon.png");
+const tray = new QSystemTrayIcon();
+tray.setIcon(trayIcon);
+tray.show();
+
+global.tray = tray; // prevents garbage collection of tray
+tray.addEventListener('activated', terminate);
+
 
 function terminate() {
-  tray.remove();
-  tray = null;
-  updateWorker.kill()
-  execSync(cmd0);
-  //gui.App.closeAllWindows();
-  //gui.App.quit();
-  //gui.process.exit(1);
-  //process.exit(1);
+  tray.delete();
+  global.tray = null;
+  updateWorker.kill();
+  process.exit(1);
 }
 
 
@@ -54,13 +48,7 @@ io.on('connection', (socket) => {
   updateWorker.on('message', (data) => {
     //let a = JSON.stringify(data.RAM[0].clk);
     socket.emit('sysinfo', data);
-})
-
-  socket.emit('ding');
-
-  socket.on('ding', () => {
-    socket.emit('dong');
-  });
+  })
 });
 
 server.listen(port, () => {
